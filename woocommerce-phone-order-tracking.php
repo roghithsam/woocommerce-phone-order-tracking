622b2567c107f0ffc5cc5cdac45991725d4f29c9
Roghithsam <91943049+roghithsam@users.noreply.github.com>
2024-12-09 20:44:26 +0530

    Update woocommerce-phone-order-tracking.php

--- woocommerce-phone-order-tracking.php
+++ woocommerce-phone-order-tracking.php
@@ -34,26 +34,26 @@ function woocommerce_phone_order_tracking_shortcode( $atts ) {
             $order = wc_get_order( apply_filters( 'woocommerce_shortcode_order_tracking_order_id', $order_id ) );
 
             if ( $order && is_a( $order, 'WC_Order' ) ) {
-				// Normalize and extract the last 10 digits
-				$normalized_input_phone = substr( preg_replace( '/\D/', '', $phone_number ), -10 );
-				$normalized_billing_phone = substr( preg_replace( '/\D/', '', $order->get_billing_phone() ), -10 );
+		// Normalize and extract the last 10 digits
+		$normalized_input_phone = substr( preg_replace( '/\D/', '', $phone_number ), -10 );
+		$normalized_billing_phone = substr( preg_replace( '/\D/', '', $order->get_billing_phone() ), -10 );
 
-				// Compare the last 10 digits
-				if ( $normalized_input_phone === $normalized_billing_phone ) {
-					do_action( 'woocommerce_track_order', $order->get_id() );
-					wc_get_template(
-						'order/tracking.php',
-						array(
-							'order' => $order,
-						)
-					);
-					return;
-				} else {
-					wc_print_notice( __( 'Sorry, the order could not be found. Please ensure the order ID and phone number are correct.', 'woocommerce' ), 'error' );
-				}
-			}else{
-				wc_print_notice( __( 'Sorry, the order could not be found. Please ensure the order ID is correct.', 'woocommerce' ), 'error' );
-			}
+		// Compare the last 10 digits
+		if ( $normalized_input_phone === $normalized_billing_phone ) {
+			do_action( 'woocommerce_track_order', $order->get_id() );
+			wc_get_template(
+				'order/tracking.php',
+				array(
+					'order' => $order,
+				)
+			);
+			return;
+		} else {
+			wc_print_notice( __( 'Sorry, the order could not be found. Please ensure the order ID and phone number are correct.', 'woocommerce' ), 'error' );
+		}
+	    }else{
+		wc_print_notice( __( 'Sorry, the order could not be found. Please ensure the order ID is correct.', 'woocommerce' ), 'error' );
+	    }
         }
     }
 
